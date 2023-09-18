311963d2c11d4b875d5e30dc953fb0b7a80a724a
VinitPania <vinit.pania@cloverinfotech.com>
2023-09-18 10:12:23 +0530

    coding in ScreenHEADDERbTN.jsx

---
+++ Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react-native';
+import {View, Image , Button ,ToastAndroid , Text , TextInput} from 'react-native'
+import { COLORS , SIZES } from './constants';
+
+const Login =  () => {
+
+    <View style = {{ flex :1 , backgroundColor : COLORS.primary}}>
+
+        <Text>Login Page </Text>
+        <TextInput>
+            
+        </TextInput>
+
+
+
+    </View>    
+
+}
